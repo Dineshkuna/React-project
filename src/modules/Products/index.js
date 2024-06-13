4aecddf694facc40e6ef10075118276494db5f66
DineshKuna <kunadinesh.work@gmail.com>
2024-06-13 14:27:01 +0530

    first commit

---
+++ src/modules/Products/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import ProductCard from "../../components/ProductCard";
+import Categories from "../../components/Categories";
+// import StatCrad from "../../components/StatCard";
+
+
+
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://fakestoreapi.com/products"
+      );
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+    return (
+    <div>
+  
+      <Categories/>
+
+      {
+        products.length > 0 ? <ProductCard products={products}/>
+        :
+        <div>Loading....</div>
+      }
+
+    </div>
+  );
+};
+
+export default Products;
